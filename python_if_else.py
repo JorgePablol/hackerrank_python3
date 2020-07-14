3f06ac21abe3ce8201549fe2e94edc1217311e56
Jorge Pablo <58957744+JorgePablol@users.noreply.github.com>
2020-07-13 19:11:21 -0500

    Create python_if_else.py

---
+++ python_if_else.py
@@ -0,0 +1,18 @@
+#Task
+#Given an integer,n , perform the following conditional actions:
+#If  is odd, print Weird
+#If  is even and in the inclusive range of 2 to 5, print Not Weird
+#If  is even and in the inclusive range of 6 to 20, print Weird
+#If  is even and greater than 20, print Not Weird
+#Link to the original page: https://www.hackerrank.com/challenges/py-if-else/problem
+
+if __name__ == '__main__':
+    n = int(input())
+    if n%2 != 0:
+        print('Weird')
+    elif n%2 == 0 and n>=2 and n <= 5:
+        print('Not Weird')
+    elif n%2 == 0 and n >= 6 and n <= 20:
+        print('Weird')
+    elif n%2 == 0 and n > 20:
+        print('Not Weird')
