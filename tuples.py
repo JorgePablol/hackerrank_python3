f1918ed4d42698165b6c253f877aeda1c5ed44a1
Jorge Pablo <58957744+JorgePablol@users.noreply.github.com>
2020-07-21 18:43:57 -0500

    Update tuples.py

--- tuples.py
+++ tuples.py
@@ -3,6 +3,8 @@
 #Task
 #Given an integer, n, and n space-separated integers as input, create a tuple, t, of those n integers.
 #Then compute and print the result of hash(t).
+#Link = https://www.hackerrank.com/challenges/python-tuples/problem
+
 
 if __name__ == '__main__':
     n = int(input())
