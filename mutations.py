3810259e7ee093fd5e063baf098d97b2c24b0520
Jorge Pablo <58957744+JorgePablol@users.noreply.github.com>
2020-07-22 18:22:37 -0500

    Create mutations.py

---
+++ mutations.py
@@ -0,0 +1,18 @@
+# MUTATIONS
+
+    # We have seen that lists are mutable (they can be changed), and tuples are immutable (they cannot be changed).
+    # Let's try to understand this with an example.
+    # You are given an immutable string, and you want to make changes to it.
+    
+    
+def mutate_string(string, position, character):
+    l = list(string)
+    l[position] = character
+    string = ''.join(l)
+    return string
+
+if __name__ == '__main__':
+    s = input()
+    i, c = input().split()
+    s_new = mutate_string(s, int(i), c)
+    print(s_new)
