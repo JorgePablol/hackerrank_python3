e4226b2bc2399f2a1322d060b0f3dc9fdbd8f49c
Jorge Pablo <58957744+JorgePablol@users.noreply.github.com>
2020-07-27 18:34:34 -0500

    Create python_evaluation.py

---
+++ python_evaluation.py
@@ -0,0 +1,10 @@
+#PYTHON EVALUATION
+    #The eval() expression is a very powerful built-in function of Python. It helps in evaluating an expression. 
+    #The expression can be a Python statement, or a code object.
+    #Link = https://www.hackerrank.com/challenges/python-eval/problem
+
+
+if __name__ == '__main__':
+
+    string_input = input()
+    eval(string_input)
