0dc51d14693b23332d006893407031bdd53d0d37
Jorge Pablo <58957744+JorgePablol@users.noreply.github.com>
2020-07-29 15:43:57 -0500

    Create re_split.py

---
+++ re_split.py
@@ -0,0 +1,11 @@
+#RE.SPLIT()
+    #You are given a string s consisting only of digits 0-9, commas , n, and dots .
+    #Your task is to complete the regex_pattern defined below, which will be used to re.split() all of the , and . symbols in s.
+    #It’s guaranteed that every comma and every dot in  is preceeded and followed by a digit.
+
+#LINK = https://www.hackerrank.com/challenges/re-split/problem  
+
+regex_pattern = r"[,.]+"	# Do not delete 'r'.
+
+import re
+print("\n".join(re.split(regex_pattern, input())))
