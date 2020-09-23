390594049f8ca3727a2605fa434cae6a6782c212
Jorge Pablo <58957744+JorgePablol@users.noreply.github.com>
2020-09-22 20:04:59 -0500

    Create mod_divmod.py

---
+++ mod_divmod.py
@@ -0,0 +1,9 @@
+# MOD DIVMOD
+# LINK = https://www.hackerrank.com/challenges/python-mod-divmod/problem
+
+a = int(input())
+b = int(input())
+c = divmod(a,b)
+d = list(c)
+[[print(i) for i in d]]
+print(c)
