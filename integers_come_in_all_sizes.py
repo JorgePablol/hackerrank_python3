1cf6d02c9f2a6a3e7f0431e69e0e3c326dcab9bb
Jorge Pablo <58957744+JorgePablol@users.noreply.github.com>
2020-09-22 20:26:37 -0500

    Create integers_come_in_all_sizes.py

---
+++ integers_come_in_all_sizes.py
@@ -0,0 +1,8 @@
+# INTEGERS COME IN ALL SIZES
+# LINK = https://www.hackerrank.com/challenges/python-integers-come-in-all-sizes/problem
+
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+print(pow(a,b)+pow(c,d))
