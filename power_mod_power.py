8ece585a51f5ef33c3b36940cca692848da63923
Jorge Pablo <58957744+JorgePablol@users.noreply.github.com>
2020-09-22 20:23:48 -0500

    Create power_mod_power.py

---
+++ power_mod_power.py
@@ -0,0 +1,11 @@
+# POWER - MOD POWERhttps://www.hackerrank.com/challenges/python-power-mod-power/problem
+# LINK = https://www.hackerrank.com/challenges/python-power-mod-power/problem
+
+a = int(input())
+b = int(input())
+c = int(input())
+
+powe = pow(a,b)
+pow_mod = pow(a,b,c)
+print(powe)
+print(pow_mod)
