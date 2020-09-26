cbaa0c5ffd24917803e65ac1e1eb657414ee6e8e
Jorge Pablo <58957744+JorgePablol@users.noreply.github.com>
2020-09-26 15:29:43 -0500

    Create defaultdict.py

---
+++ defaultdict.py
@@ -0,0 +1,15 @@
+# DEFAULTDICT TUTORIAL
+# LINK = https://www.hackerrank.com/challenges/defaultdict-tutorial/problem
+
+
+
+from collections import defaultdict
+
+d = defaultdict(list)
+n, m = map(int, input().split())
+
+for i in range(n):
+    d[input()].append(str(i + 1))
+    
+for j in range(m):
+    print(' '.join(d[input()]) or -1)
